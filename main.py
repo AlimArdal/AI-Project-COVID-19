39dffb2ad5265b3214f8bfdbca7e95131cd9dd8d
Alim Ardal <alim.ardal@gmail.com>
2023-05-30 00:37:01 +0200

    V2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-#import seaborn as sns; sns.set()
+from datetime import datetime
 
-
-#a=sns.distplot(data)
+def parse_date(date_string):
+    try:
+        return pd.to_datetime(date_string, format='%m/%d/%Y')
+    except ValueError:
+        return pd.to_datetime(date_string, format='%d-%m-%Y')
 
 # COVID19_line_list_data.csv
 print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
@@ -11,6 +14,60 @@ print("-------------------------------------------------------------------------
 data = pd.read_csv('COVID19_line_list_data.csv')
 print(data)
 print("----------------------------------------------")
+
+# DATES --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def is_bad_date(date_string):
+    if not isinstance(date_string, str):
+        return True
+    try:
+        datetime.strptime(date_string, date_format)
+        return False
+    except ValueError:
+        return True
+    
+data['reporting date'] = data['reporting date'].apply(parse_date)
+data['reporting date'] = data['reporting date'].dt.strftime('%d/%m/%Y')
+data['reporting date'] = pd.to_datetime(data['reporting date'], format='%d/%m/%Y')
+mean_date = data['reporting date'].mean()
+data['reporting date'] = data['reporting date'].fillna(mean_date)
+data['reporting date'] = data['reporting date'].apply(lambda x: x.toordinal())
+print(data['reporting date'])
+
+data['symptom_onset'] = data['symptom_onset'].apply(parse_date)
+data['symptom_onset'] = data['symptom_onset'].dt.strftime('%d/%m/%Y')
+data['symptom_onset'] = pd.to_datetime(data['symptom_onset'], format='%d/%m/%Y')
+median_date = data['symptom_onset'].dropna().median()
+data['symptom_onset'] = data['symptom_onset'].fillna(median_date)
+data['symptom_onset'] = data['symptom_onset'].apply(lambda x: x.toordinal())
+
+date_format = "%d-%m-%Y"
+bad_dates = data['hosp_visit_date'].apply(is_bad_date)
+data = data.loc[~bad_dates]
+
+data['hosp_visit_date'] = data['hosp_visit_date'].apply(parse_date)
+data['hosp_visit_date'] = data['hosp_visit_date'].dt.strftime('%d/%m/%Y')
+data['hosp_visit_date'] = pd.to_datetime(data['hosp_visit_date'], format='%d/%m/%Y')
+median_date2 = data['hosp_visit_date'].dropna().median()
+data['hosp_visit_date'] = data['hosp_visit_date'].fillna(median_date2)
+data['hosp_visit_date'] = data['hosp_visit_date'].apply(lambda x: x.toordinal())
+
+data['exposure_start'] = data['exposure_start'].apply(parse_date)
+data['exposure_start'] = data['exposure_start'].dt.strftime('%d/%m/%Y')
+data['exposure_start'] = pd.to_datetime(data['exposure_start'], format='%d/%m/%Y')
+median_date3 = data['exposure_start'].dropna().median()
+data['exposure_start'] = data['exposure_start'].fillna(median_date3)
+data['exposure_start'] = data['exposure_start'].apply(lambda x: x.toordinal())
+
+data['exposure_end'] = data['exposure_end'].apply(parse_date)
+data['exposure_end'] = data['exposure_end'].dt.strftime('%d/%m/%Y')
+data['exposure_end'] = pd.to_datetime(data['exposure_end'], format='%d/%m/%Y')
+median_date4 = data['exposure_end'].dropna().median()
+data['exposure_end'] = data['exposure_end'].fillna(median_date4)
+data['exposure_end'] = data['exposure_end'].apply(lambda x: x.toordinal())
+
+# DATES --------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 # Remplacer les valeurs non numériques dans la colonne 'age' et 'death' par la médiane de cette colonne
@@ -43,16 +100,25 @@ print(f"La corrélation entre l'âge et la mort est de {corr:.2f}")
 print("----------------------------------------------")
 
 
-# Supprimer les lignes contenant des valeurs manquantes
-data = data.dropna()
-print(data)
+corr7 = data[['age','recovered','visiting Wuhan','from Wuhan','case_in_country','If_onset_approximated','death','reporting date','symptom_onset','hosp_visit_date','exposure_start','exposure_end']].corr().apply(lambda x: round(x, 2))
+
+corr2 = data['age'].corr(data['recovered'])
+corr3 = data['age'].corr(data['visiting Wuhan'])
+corr4 = data['age'].corr(data['from Wuhan'])
+corr5 = data['visiting Wuhan'].corr(data['from Wuhan'])
+
+print(corr7)
 print("----------------------------------------------")
 
-data2=data.values().astype(str)
-print (data2)
+
+# Supprimer les lignes contenant des valeurs manquantes
+#data = data.dropna()
+#print(data)
+#print("----------------------------------------------")
+
 
 # Calculer la matrice de corrélation
-corr_matrix = data2.corr()
+corr_matrix = data.corr()
 print(corr_matrix)
 
 # Afficher les corrélations avec la variable cible
